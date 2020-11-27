13e3f70c7f43b140dfc3d3f823a9d24747aa582c
刘静静 <33412221+liujingjingg@users.noreply.github.com>
2020-11-27 14:47:54 +0800

    fix: username & password validator update (#12)
    * fix: role & role-management update
    
    * fix: change fileName
    
    * fix: userSelectMultiple limit update
    
    * fix: add selectedRowKeys
    
    * fix: add validator
    
    * fix：password plcaeholder update

--- ProfileForm/index.tsx
+++ ProfileForm/index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  Form, Input, Switch, Icon,
+  Form, Input, Switch, Icon
 } from 'antd';
 import { FormattedMessage } from 'react-intl';
 import { FormProps } from 'antd/lib/form';
@@ -30,6 +30,28 @@ class ProfileForm extends Component<Props & FormProps> {
     return this.props.form!.validateFields;
   }
 
+  validateUserName = (rule: string, value: string, callback: any) => {
+    const regex = /^[a-zA-Z][a-zA-Z0-9_]{3,29}$/
+    if (!regex.test(value) && value !== '') {
+      callback('请输入小于30字符，大于3字符，限字符a-z,A-Z,下划线，以字母开头的用户名!');
+    }
+    callback();
+  }
+
+  validatePassword = (rule: string, value: string, callback: any) => {
+    const passwordReg = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*,\.])[0-9a-zA-Z!@#$%^&*,\\.].*$/
+
+    if (value) {
+      if (!passwordReg.test(value)) {
+        callback('密码必须同时包含大写字母、小写字母、数字和符号且长度为8~20个字符！');
+      }
+      if (value.length < 8 || value.length > 20) {
+        callback('密码长度8~20位')
+      }
+    }
+    callback();
+  }
+
   render() {
     const { type, isrootVsible, initialValue } = this.props;
     const { getFieldDecorator } = this.props.form!;
@@ -39,9 +61,12 @@ class ProfileForm extends Component<Props & FormProps> {
         <FormItem label={<FormattedMessage id="user.username" />} required>
           {getFieldDecorator('username', {
             initialValue: initialValue.username,
-            rules: [{ required: true }],
+            rules: [{ required: true }, { validator: this.validateUserName }],
           })(
-            <Input autoComplete="off" disabled={type === 'put'} />,
+            <Input
+              autoComplete="off"
+              disabled={type === 'put'}
+              placeholder="小于30字符，大于3字符，限字符a-z,A-Z,下划线，请以字母开头" />,
           )}
         </FormItem>
         {
@@ -49,12 +74,15 @@ class ProfileForm extends Component<Props & FormProps> {
             ? (
               <>
                 <FormItem label={<FormattedMessage id="user.password" />} required>
-                  {getFieldDecorator('password', {
-                    rules: [{ required: true }],
-                  })(
-                    <Input type="password" autoComplete="new-password" />,
-                  )}
-                </FormItem>
+                    {getFieldDecorator('password', {
+                      rules: [{ required: true }, { validator: this.validatePassword }],
+                    })(
+                      <Input
+                        type="password"
+                        autoComplete="new-password"
+                        placeholder="长度8~20字符，必须同时包含大写字母、小写字母、数字和符号！" />,
+                    )}
+                  </FormItem>
               </>
             ) : null
         }
