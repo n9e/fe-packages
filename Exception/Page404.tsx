ea0aa3bafa37a38da8e33e3136795217ac62dfef
liujingjingg <949694044@qq.com>
2020-12-30 22:26:46 +0800

    style: 404 style update

--- Exception/Page404.tsx
+++ Exception/Page404.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Row, Col } from 'antd';
 import { RouteComponentProps } from 'react-router-dom';
 
+const img404 = require('./imgs/403Img.png');
 
 const Page404: React.FC<RouteComponentProps> = (props) => {
   const { history } = props;
   const prefixCls = 'ecmc-exception';
+
   return (
-    <div className={prefixCls}>
+    <div className={`${prefixCls} ${prefixCls}-403`}>
       <div className={`${prefixCls}-main`}>
-        <div className={`${prefixCls}-title`}>404</div>
-        <div className={`${prefixCls}-content mb10`}>抱歉，你访问的页面不存在</div>
-        <Button
-          icon="arrow-left"
-          type="primary"
-          onClick={() => {
-            history.push({
-              pathname: '/',
-            });
-          }}
-        >
-          返回首页
-        </Button>
+        <Row gutter={200}>
+          <Col span={12}>
+            <img src={img404} alt="404" />
+          </Col>
+          <Col span={12}>
+            <div className={`${prefixCls}-title`}>404</div>
+            <div className={`${prefixCls}-content mb10`}>抱歉，你访问的页面不存在</div>
+            <Button
+              className={`${prefixCls}-btn`}
+              type="primary"
+              onClick={() => {
+                history.push({
+                  pathname: '/',
+                });
+              }}
+            >
+              返回首页
+                </Button>
+          </Col>
+        </Row>
       </div>
     </div>
   );
