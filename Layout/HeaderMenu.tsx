4d5930be8cd0550db278476c618f171e693e1afd
liujingjingg <949694044@qq.com>
2021-01-06 19:04:54 +0800

    fix: add menusInter

--- Layout/HeaderMenu.tsx
+++ Layout/HeaderMenu.tsx
@@ -6,10 +6,11 @@ import { getIntl } from '../hooks/useFormatMessage';
 import { prefixCls } from './config';
 import StarMenus from './StarMenus';
 import { isAbsolutePath } from './utils';
+import request from '@pkgs/request';
+import api from '@pkgs/api';
 
 const cPrefixCls = `${prefixCls}-layout`;
 const { Sider, Content } = Layout;
-
 
 export default function HeaderMenu(props: any) {
   const { locale } = getIntl();
@@ -19,14 +20,19 @@ export default function HeaderMenu(props: any) {
   const [queryParams, setQueryParams] = useState('');
   const [historyList, setHistoryList] = useState([]);
 
-  const showMenus = useMemo(() => menus.map((item: any) =>
-    ({
-      ...item, children: item?.children.filter((item: any) =>
-        item.name.includes(queryParams) || !queryParams
-      )
-    })).filter((item: any) => item.children.length > 0)
-    , [queryParams, menus]
+  const showMenus = useMemo(
+    () =>
+      menus
+        .map((item: any) => ({
+          ...item,
+          children: item?.children?.filter(
+            (item: any) => item.name.includes(queryParams) || !queryParams
+          ),
+        }))
+        ?.filter((item: any) => item.children?.length > 0),
+    [queryParams, menus]
   );
+
   const setLocal = (name: any) => {
     setStars(name);
     const jsonArrayString = JSON.stringify(name);
@@ -85,7 +91,6 @@ export default function HeaderMenu(props: any) {
     } catch (e) {
       console.log('历史菜单未缓存，或解析缓存数据失败');
     }
-
     if (defaultStars.length) {
       setStars(defaultStars);
     }
@@ -94,83 +99,110 @@ export default function HeaderMenu(props: any) {
     if (defaultHistory.length) {
       setHistoryList(defaultHistory);
     }
-    fetch('/static/menusConfig.json')
+    fetch('/static/feConfig.json')
       .then((res) => {
         return res.json();
       })
       .then((res) => {
-        setMenus(res);
+        res.header.mode === 'complicated'
+          ? request(`${api.menus}?onlyOnlineService=true`).then((res) => {
+              const menus = res.map((item: any) => ({
+                name: item.displayName,
+                nameEn: item.code,
+                children: item?.misService?.map((item: any) => ({
+                  name: item.displayName,
+                  path: item.applyUrl,
+                  nameEn: item.code,
+                })),
+              }));
+              setMenus(menus);
+            })
+          : fetch('/static/menusConfig.json')
+              .then((res) => {
+                return res.json();
+              })
+              .then((res) => {
+                setMenus(res);
+              });
       });
   }, []);
-
   const hasChildren = (menus: any): boolean => {
     let lock = false;
     menus.map((item: any) => {
-      item?.children.length !== 0 && (lock = true)
-    })
+      item?.children.length !== 0 && (lock = true);
+    });
     return lock;
-  }
+  };
 
   const renderContentMenus = (menus: any[]) => {
-    return hasChildren(menus) ? _.map(menus, (menu) => {
-      return (
-        <dl
-          key={menu.name}
-          className={classnames({
-            [`${cPrefixCls}-menus-content-menu-group`]: true,
-            [`${cPrefixCls}-menus-content-menu-group-history`]:
-              menu.name === '最近访问',
-          })}
-        >
-          <dt className={`${cPrefixCls}-menus-content-menu-group-title`}>
-            {locale === 'en' ? menu.nameEn : menu.name}
-          </dt>
-          {_.map(menu.children, (item) => {
-            const stared = !!_.find(stars, { name: item.name });
-            return (
-              <dd
-                key={item.name}
-                className={classnames({
-                  [`${cPrefixCls}-menus-content-menu`]: true,
-                  [`${cPrefixCls}-menus-content-starMenu`]: stared,
-                })}
-              >
-                <a
-                  href={isAbsolutePath(item.path) ? item.path : `/${item.path}`}
-                  target={item.target}
-                  onClick={() => {
-                    let newHistory = _.concat(item, historyList);
-                    let newArr = _.filter(newHistory, (item, index, arr) => {
-                      return _.findIndex(arr, item) === index;
-                    })
-                    setHistoryLocal(newArr);
-                  }}
+    return hasChildren(menus) ? (
+      _.map(menus, (menu) => {
+        return (
+          <dl
+            key={menu.name}
+            className={classnames({
+              [`${cPrefixCls}-menus-content-menu-group`]: true,
+              [`${cPrefixCls}-menus-content-menu-group-history`]:
+                menu.name === '最近访问',
+            })}
+          >
+            <dt className={`${cPrefixCls}-menus-content-menu-group-title`}>
+              {locale === 'en' ? menu.nameEn : menu.name}
+            </dt>
+            {_.map(menu.children, (item) => {
+              const stared = !!_.find(stars, { name: item.name });
+              return (
+                <dd
+                  key={item.name}
+                  className={classnames({
+                    [`${cPrefixCls}-menus-content-menu`]: true,
+                    [`${cPrefixCls}-menus-content-starMenu`]: stared,
+                  })}
                 >
-                  {locale === 'en' ? item.nameEn : item.name}
-                </a>
-                <Icon
-                  title={stared ? '取消收藏' : '添加收藏'}
-                  type="star"
-                  className={`${cPrefixCls}-menus-content-menu-star`}
-                  theme={stared ? 'filled' : 'outlined'}
-                  onClick={() => {
-                    let newStars;
-                    if (stared) {
-                      newStars = _.remove(stars, (star) => {
-                        return star.path !== item.path;
-                      });
-                    } else {
-                      newStars = _.concat(stars, item);
+                  <a
+                    href={
+                      isAbsolutePath(item.path) ? item.path : `/${item.path}`
                     }
-                    setLocal(newStars);
-                  }}
-                />
-              </dd>
-            );
-          })}
-        </dl>
-      );
-    }) : <div style={{ color: '#333', fontSize: 14, marginTop: 20 }}>未找到与"<span style={{ color: '#FB4E57' }}>{queryParams}</span>"相关的产品</div>;
+                    target={item.target}
+                    onClick={() => {
+                      let newHistory = _.concat(item, historyList);
+                      let newArr = _.filter(newHistory, (item, index, arr) => {
+                        return _.findIndex(arr, item) === index;
+                      });
+                      setHistoryLocal(newArr);
+                    }}
+                  >
+                    {locale === 'en' ? item.nameEn : item.name}
+                  </a>
+                  <Icon
+                    title={stared ? '取消收藏' : '添加收藏'}
+                    type="star"
+                    className={`${cPrefixCls}-menus-content-menu-star`}
+                    theme={stared ? 'filled' : 'outlined'}
+                    onClick={() => {
+                      let newStars;
+                      if (stared) {
+                        newStars = _.remove(stars, (star) => {
+                          return star.path !== item.path;
+                        });
+                      } else {
+                        newStars = _.concat(stars, item);
+                      }
+                      setLocal(newStars);
+                    }}
+                  />
+                </dd>
+              );
+            })}
+          </dl>
+        );
+      })
+    ) : (
+      <div style={{ color: '#333', fontSize: 14, marginTop: 20 }}>
+        未找到与"<span style={{ color: '#FB4E57' }}>{queryParams}</span>
+        "相关的产品
+      </div>
+    );
   };
 
   return (
@@ -218,14 +250,13 @@ export default function HeaderMenu(props: any) {
             placeholder="请输入关键词"
             onChange={(e) => {
               setQueryParams(e.target.value);
-              e.target.value === '' ? setIcon(false) : setIcon(true)
+              e.target.value === '' ? setIcon(false) : setIcon(true);
             }}
           />
         </div>
         <div className={`${cPrefixCls}-menus-content-menus`}>
           {renderContentMenus(showMenus)}
         </div>
-
         <Icon
           type="close"
           className={`${cPrefixCls}-menus-close`}
