055827f67314ab933f8cc85767d5e59ea904c46b
liujingjingg <949694044@qq.com>
2021-02-05 20:33:29 +0800

    feat: add prvi api

--- api.tsx
+++ api.tsx
@@ -53,6 +53,8 @@ const api = {
   organization: getRDBApi('/tree/orgs'),
   privileges: getRDBApi('/privileges'),
   pwdRules: getRDBApi('/pwd-rules'),
+  privilegesWeights:getRDBApi('/privileges-weight'),
+  privilegesImport:getRDBApi('/privileges-import'),
 
 
   host: getAMSCEApi('/host'),
