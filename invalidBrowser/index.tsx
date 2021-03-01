59523615f68655ef45388794d0df193a27e9b439
daixiaqing <xqing9024@gmail.com>
2021-03-01 18:01:24 +0800

    feat: support more browsers

--- invalidBrowser/index.tsx
+++ invalidBrowser/index.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import { Modal } from 'antd';
 import * as Bowser from 'bowser';
-import api from '../api';
+import _ from 'lodash';
 import './style.less';
 
 const browser = Bowser.getParser(window.navigator.userAgent);
-export const isValidBrowser = browser.satisfies({
+const supportedBrowser = {
   chrome: '>=80',
-  edge: '>=84',
-});
+  edge: '>=80',
+  firefox: '>=80',
+  safari: '>=13',
+};
+export const isValidBrowser = browser.satisfies(supportedBrowser);
 
-function invalidBrowserModal(downloadBrowserUrl?: { edge: string, chrome: string }) {
+function invalidBrowserModal() {
   Modal.warning({
     width: 600,
     title: <p className="invalid-browse-modal-content-top">Hi~,您的浏览器版本过低</p>,
     content: (
       <div className="invalid-browse-modal-content">
+        <p className="invalid-browse-modal-content-center">
+          当前浏览器是：
+          {browser.getBrowserName()}
+          {' '}
+          {browser.getBrowserVersion()}
+        </p>
+        <p className="invalid-browse-modal-content-center">
+          页面正常访问需要：
+          {
+            _.join(_.map(supportedBrowser, (version, name) => `${name} ${version}`), ', ')
+          }
+        </p>
         <p className="invalid-browse-modal-content-center">建议您对浏览器进行升级，以便获得更好的使用体验。</p>
-        <p className="invalid-browse-modal-content-backColor" />
-        <p className="invalid-browse-modal-content-bottom">推荐以下浏览器和版本</p>
-        {
-          downloadBrowserUrl ?
-            <div className="invalid-browse-modal-bottom">
-              <a href={downloadBrowserUrl.edge}>
-                <img alt="IE Edge" src={require('./imgs/IE.png')} />
-                <p>IE Edge</p>
-              </a>
-              <a href={downloadBrowserUrl.chrome}>
-                <img alt="Google Chrome" src={require('./imgs/google.png')} />
-                <p>Google Chrome</p>
-              </a>
-            </div> : null
-        }
       </div>
     ),
     okButtonProps: {
@@ -44,13 +44,6 @@ function invalidBrowserModal(downloadBrowserUrl?: { edge: string, chrome: string
 
 export default function invalidBrowser() {
   if (!isValidBrowser) {
-    fetch(api.downloadBrowser).then((res) => {
-      return res.json();
-    }).then((res) => {
-      invalidBrowserModal(res.dat);
-    }).catch((e) => {
-      console.log(e);
-      invalidBrowserModal();
-    });
+    invalidBrowserModal();
   }
 }
